989a92767dc7d0f965b12c995b5e0f0f2469aae0
nargess alavi <alavi_n70@yahoo.com>
2024-07-19 09:14:22 +0330

    feat
    start

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Navbar() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div>
+      <nav className="nav-bar">
+        <div className="logo">
+          <span role="img">🍿</span>
+          <h1>usePopcorn</h1>
+        </div>
+        <input
+          className="search"
+          type="text"
+          placeholder="Search movies..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <p className="num-results">
+          Found <strong>x</strong> results
+        </p>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
